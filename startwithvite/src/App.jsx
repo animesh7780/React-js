e2e02f01a8163539aa724a4e1bfff27a2e6108e5
animesh7780 <thakuranimesh235@gmail.com>
2025-01-03 11:34:37 +0530

    update

--- startwithvite/src/App.jsx
+++ startwithvite/src/App.jsx
@@ -1,11 +1,27 @@
 import First from "./first"
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+const username = "Ani"
+
 function App() {
   return (
     <>
     <First/>
+    <MyApp/>
     <h1>Hello</h1>
     <h2>Badhiya Sab</h2>
+    <h3>Ab banega project wit react wit {username}</h3>
+    <p>This is a paragraph</p>
+    <br />
+    <hr />
+    <input type="text" placeholder="Enter your name" />
     </>
   )
 }
--- startwithvite/src/main.jsx
+++ startwithvite/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// const ReactElement = {
+//   type: 'a',
+//   props:{
+//       href: 'https://www.google.com',
+//       target: '_blank',
+//   },
+//   children: ['click me to visit @ google']
+// }
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank',
+  },
+  'click me to visit @ google',
+
+)
+
+const anotherElement = (
+  <a href="google.com"  target="_blank">Visit google</a>
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App/>
 )
