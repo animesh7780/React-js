d3b12224467ad30c8315853e7d1c998d3a53f5f8
animesh7780 <thakuranimesh235@gmail.com>
2025-01-10 08:54:01 +0530

    update

--- Test/src/App.jsx
+++ Test/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   const [input, setInput] = useState('');
 
   const handleClick = () => {
+    if(!input){
+      alert("Chal Bhag jaa yahan se")
+      return
+    }
     const completeUrl = url + input; 
     window.open(completeUrl, '_blank'); 
   };
@@ -12,12 +16,17 @@ function App() {
   return (
     <div>
       <input
+        className="border border-black/10 rounded-lg px-3 py-1.5"
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Enter search term"
       />
-      <button onClick={handleClick}>Search</button>
+      <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+      onClick={handleClick}>
+        Search
+        </button>
     </div>
   );
 }
